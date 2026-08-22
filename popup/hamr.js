2e38977aaed98249b8d16a0dfc5f6f60554dff62
elioat <elioatroo42@gmail.com>
2026-08-22 00:23:10 -0500

    qr code is working todo: add buttons

--- popup/hamr.js
+++ popup/hamr.js
@@ -1,27 +1,45 @@
-import { compress, decompress } from "./compress";
+import { compress, decompress } from "../hamr/compress.js";
 import {
   outputAlphabetASCII,
   outputAlphabetQR,
   outputAlphabetEmoji,
-} from "./alphabet";
+} from "../hamr/alphabet.js";
+import {
+  generate as qrGen,
+  mode as qrMode,
+  correction as qrCorrect,
+} from "../lean-qr/lean-qr.js";
 
-function getPage() {
-  browser.tabs.query({ currentWindow: true, active: true }).then((tabs) => {
-    return tabs[0].url;
-  });
+//const compressedElement = document.querySelector("#compressed-url");
+const qrCode = document.querySelector("#qrcode");
+
+function update(tabs) {
+  const url = tabs[0].url;
+  //const compressedPayLoad = compress(url, outputAlphabetASCII);
+
+  //compressedElement.textContent = `http://ha.mr#${compressedPayLoad}`;
+  //compressedElement.href = `http://ha.mr#${compressedPayLoad}`;
+
+  const qrURL = `HTTP://HA.MR/${compress(url, outputAlphabetQR)}`;
+  const qr = qrGen(
+    qrMode.alphaNumeric(qrURL),
+    {
+      minVersion: 1,
+      maxVersion: 40,
+      minCorrectionLevel: qrCorrect.M,
+      maxCorrectionLevel: qrCorrect.H
+    });
+  qr.toCanvas(qrCode,
+    {
+      on:  [0x00, 0x00, 0x00, 0xFF], // black
+      off: [0xFF, 0xFF, 0xFF, 0xFF], // white
+      pad: 2,
+    }
+  );
+  qrCode.title = qrURL;
+
 }
 
-const urlElement = document.querySelector("#current-url");
-const compressedElement = document.querySelector("#compressed-url");
-
 (() => {
-
-let url = getPage();
-let compressedPayLoad = compress(url, outputAlphabetASCII);
-
-urlElement.textContent = url;
-urlElement.href = url;
-compressedElement.textContent = `http://ha.mr#${compressedPayLoad}`;
-compressedElement.href = `http://ha.mr#${compressedPayLoad}`;
-
+  browser.tabs.query({ currentWindow: true, active: true }, update);
 })();
