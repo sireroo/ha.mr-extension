1403fb84ce6a41d3876f54a8363e8fe7e70d177c
elioat <elioatroo42@gmail.com>
2026-08-24 02:48:47 -0500

    mvp done todo: clean up anything (importing a font thats never used) and fix icon in bar

---
+++ README.md
@@ -0,0 +1,9 @@
+# HA.MR QR CODE EXTENSION
+
+code copied from <ha.mr>()
+
+copied form ha.mr
+
+lean-qr
+tabler icons
+twemoji
--- popup/hamr.js
+++ popup/hamr.js
@@ -10,34 +10,85 @@ import {
   correction as qrCorrect,
 } from "../lean-qr/lean-qr.js";
 
-//const compressedElement = document.querySelector("#compressed-url");
 const qrCode = document.querySelector("#qrcode");
+const linkButton = document.querySelector("#urlbtn");
+const emojiButton = document.querySelector("#emjbtn");
+const qrButton = document.querySelector("#qrbtn");
+const saveLink = document.querySelector("#svelink");
+const saveButton = document.querySelector("#svebtn");
+
+function flash(element) {
+  element.style.animation = "none";
+  element.offsetHeight;
+  element.style.animation = null;
+}
 
 function update(tabs) {
-  const url = tabs[0].url;
-  //const compressedPayLoad = compress(url, outputAlphabetASCII);
+  linkButton.style.animation = "none";
+  emojiButton.style.animation = "none";
+  qrButton.style.animation = "none";
+  saveButton.style.animation = "none";
 
-  //compressedElement.textContent = `http://ha.mr#${compressedPayLoad}`;
-  //compressedElement.href = `http://ha.mr#${compressedPayLoad}`;
+  const url = tabs[0].url;
+  const ASCIILink = `http://ha.mr#${compress(url, outputAlphabetASCII)}`;
+  const emojiLink = `http://ha.mr#${compress(url, outputAlphabetEmoji)}`;
 
   const qrURL = `HTTP://HA.MR/${compress(url, outputAlphabetQR)}`;
-  const qr = qrGen(
-    qrMode.alphaNumeric(qrURL),
-    {
-      minVersion: 1,
-      maxVersion: 40,
-      minCorrectionLevel: qrCorrect.M,
-      maxCorrectionLevel: qrCorrect.H
-    });
-  qr.toCanvas(qrCode,
-    {
-      on:  [0x00, 0x00, 0x00, 0xFF], // black
-      off: [0xFF, 0xFF, 0xFF, 0xFF], // white
-      pad: 2,
-    }
-  );
+  const qr = qrGen(qrMode.alphaNumeric(qrURL), {
+    minVersion: 1,
+    maxVersion: 40,
+    minCorrectionLevel: qrCorrect.M,
+    maxCorrectionLevel: qrCorrect.H,
+  });
+  const qrDataURL = qr.toDataURL({
+    type: "image/png",
+    on: [0x00, 0x00, 0x00, 0xff], // black
+    off: [0xff, 0xff, 0xff, 0xff], // white
+    pad: 2,
+    scale: 8,
+  });
+  qrCode.src = qrDataURL;
   qrCode.title = qrURL;
+  saveLink.href = qrDataURL;
 
+  linkButton.addEventListener("click", async () => {
+    try {
+      await navigator.clipboard.writeText(ASCIILink);
+      console.log("Link copied!");
+      flash(linkButton);
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
+  });
+
+  emojiButton.addEventListener("click", async () => {
+    try {
+      await navigator.clipboard.writeText(emojiLink);
+      console.log("Link copied!");
+      flash(emojiButton);
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
+  });
+
+  qrButton.addEventListener("click", async () => {
+    try {
+      const qrBlob = await fetch(qrDataURL).then(response => {return response.blob()});
+      await navigator.clipboard.write([
+        new ClipboardItem({
+          "image/png": qrBlob,
+        }),
+      ]);
+      console.log("Image copied!");
+      flash(qrButton);
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
+  });
+
+  saveButton.addEventListener("click", async () => {
+    flash(saveButton);
+  });
 }
 
 (() => {
